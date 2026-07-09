a241538db05c766b194b08c0d8ddddfa66a7101f
xrui94 <ruixing94@outlook.com>
2026-07-09 16:40:16 +0800

    fix: 版本号统一为 0.1.0

--- libs/random/include/cckit/version.h
+++ libs/random/include/cckit/version.h
@@ -1,7 +1,7 @@
 // version.h - 版本信息
 #pragma once
 
-#define CCKIT_VERSION_MAJOR 1
-#define CCKIT_VERSION_MINOR 0
+#define CCKIT_VERSION_MAJOR 0
+#define CCKIT_VERSION_MINOR 1
 #define CCKIT_VERSION_PATCH 0
-#define CCKIT_VERSION_STRING "1.0.0"
+#define CCKIT_VERSION_STRING "0.1.0"
